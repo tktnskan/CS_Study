8cd6aa0ff95bf8889aa15650eefe935771b1ed35
mynameisdaun <daun9870.jung@gmail.com>
2021-10-23 12:12:35 +0900

    Map sorting

--- Java:Sorting.java
+++ Java:Sorting.java
@@ -12,3 +12,16 @@ Java Sorting
 
         Character[] charObjectArray = str.chars().mapToObj(c -> (char)c).toArray(Character[]::new);
         Arrays.sort(charObjectArray,Collections.reverseOrder());
+
+
+2. Map sorting
+/*https://stackoverflow.com/questions/109383/sort-a-mapkey-value-by-values*/
+* Value 오름차순
+Stream<Map.Entry<K,V>> sorted =
+                                map.entrySet().stream()
+                                   .sorted(Map.Entry.comparingByValue());
+
+* Value 내림차순
+Stream<Map.Entry<K,V>> sorted =
+                                map.entrySet().stream()
+                                   .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()));
